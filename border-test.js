b8dbb6f155f7ceec87b2bca9ae915370beea874d
yedead1 <yedead1@hotmail.com>
2020-12-25 23:42:19 +0000

    Update border-test.js

--- border-test.js
+++ border-test.js
@@ -1,7 +1,6 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Used to change the text-shadow of an element based upon the average color intensity
+ * of a image behind the text. Though it could be used todo more as it just changes a css class.
  */
 function getPixel(imgData, index) {
   return imgData.data.subarray(index*4, index*4+4); // Uint8ClampedArray(4) [R,G,B,A]
@@ -23,10 +22,13 @@ function getPixelXY(imgData, x, y) {
             cvs.height = $(this).height();
 
             var ctx = cvs.getContext('2d');
+            //Clip the image down to the elements size and position on the page
             ctx.drawImage(img, pos.left, pos.top, cvs.width, cvs.height, 0, 0, cvs.width, cvs.height);
 
             var idt = ctx.getImageData(0, 0, cvs.width, cvs.height);
             var sum =0;
+            //Could be more efficient to check every other pixel ie x+=5 and y+=3 etc.
+            //not really seen any major slow downs as is in my tests
             for (var x=0;x<cvs.width;x++){
                 for (var y=0;y<cvs.height;y++){
                     var c = getPixelXY(idt, x, y);
@@ -34,7 +36,7 @@ function getPixelXY(imgData, x, y) {
                 }
             }   
             var avg = sum /= cvs.width * cvs.height;
-            $(this).removeClass('lightbg');
+            $(this).removeClass('lightbg'); //Initial class used if js not enabled, replace with class checks if needed
             if (avg > 382.5){    //765 combined rgb val, 382.5 is the mid val
                 //$(this).css('text-shadow: -1px 0 #000, 0 1px #000, 1px 0 #000, 0 -1px #000;');
                 $(this).addClass('lightbg');
@@ -43,7 +45,7 @@ function getPixelXY(imgData, x, y) {
                 //$(this).css('text-shadow: -1px 0 #fff, 0 1px #fff, 1px 0 #fff, 0 -1px #fff !important;');
                 $(this).addClass('darkbg');
             }
-            $(document).remove('canvas');
+            $(document).remove('canvas'); //Done with the canvas element so just remove it from the DOM
         });
     }
 })(jQuery);
